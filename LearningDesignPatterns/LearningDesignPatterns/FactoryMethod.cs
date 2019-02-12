e5c1718da85e4d47ccd6758633fb9a5637dbece5
Nitin Paul <paulnitin@gmail.com>
2019-02-12 21:50:14 +0530

    Inner Factory

--- LearningDesignPatterns/LearningDesignPatterns/FactoryMethod.cs
+++ LearningDesignPatterns/LearningDesignPatterns/FactoryMethod.cs
@@ -2,28 +2,12 @@
 
 namespace LearningDesignPatterns
 {
-    /// <summary>
-    /// Learning the Factory method pattern
-    /// </summary>
-    public static class PointFactory
-    {
-        public static Point NewCartesianPoint(double x, double y)
-        {
-            return new Point(x, y);
-        }
-
-        public static Point NewPolarPoint(double rho, double theta)
-        {
-            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-        }
-    }
-
-    public class Point
+   public class Point
     {
         // factory method
         private double x, y;
 
-        public Point(double x, double y)
+        private Point(double x, double y)
         {
             this.x = x;
             this.y = y;
@@ -32,6 +16,19 @@ namespace LearningDesignPatterns
         public override string ToString()
         {
             return $"{nameof(x)} : {x}, {nameof(y)} : {y}";
+        }
+
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
         }
     }
 
@@ -45,10 +42,10 @@ namespace LearningDesignPatterns
             //var cartpoint = Point.NewCartesianPoint(10, 20);
             //Console.WriteLine("Cartesian point is {0}", cartpoint.ToString());
 
-            var point = PointFactory.NewPolarPoint(10, 20);
+            var point = Point.Factory.NewPolarPoint(10, 20);
             Console.WriteLine("Polar point is {0}", point.ToString());
 
-            var cartpoint = PointFactory.NewCartesianPoint(10, 20);
+            var cartpoint = Point.Factory.NewCartesianPoint(10, 20);
             Console.WriteLine("Cartesian point is {0}", cartpoint.ToString());
 
             Console.ReadLine();
