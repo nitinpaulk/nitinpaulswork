9b7a834eb3acfcc579749f37f3e84784caa362d9
Nitin Paul <paulnitin@gmail.com>
2019-02-18 06:04:54 +0530

    More Design patterns

---
+++ LearningDesignPatterns/LearningDesignPatterns/AbstractFactory1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    /// <summary>
+    /// An abstract product.
+    /// </summary>
+    abstract class Sandwich { }
+
+    /// <summary>
+    /// An abstract product.
+    /// </summary>
+    abstract class Dessert { }
+
+    /// <summary>
+    /// The Abstract Factory class, which defines methods for creating abstract objects.
+    /// </summary>
+    abstract class RecipeFactory
+    {
+        public abstract Sandwich CreateSandwich();
+        public abstract Dessert CreateDessert();
+    }
+
+    /// <summary>
+    /// A concrete product
+    /// </summary>
+    class BLT : Sandwich { }
+
+    /// <summary>
+    /// A concrete product
+    /// </summary>
+    class CremeBrulee : Dessert { }
+
+
+    /// <summary>
+    /// A concrete factory which creates concrete objects by implementing the abstract factory's methods.
+    /// </summary>
+    class AdultCuisineFactory : RecipeFactory
+    {
+        public override Sandwich CreateSandwich()
+        {
+            return new BLT();
+        }
+
+        public override Dessert CreateDessert()
+        {
+            return new CremeBrulee();
+        }
+    }
+
+    /// <summary>
+    /// A concrete product
+    /// </summary>
+    class PBandJ : Sandwich { }
+
+    /// <summary>
+    /// A concrete product
+    /// </summary>
+    class IceCreamSundae : Dessert { }
+
+    /// <summary>
+    /// A concrete factory which creates concrete objects by implementing the abstract factory's methods.
+    /// </summary>
+    class KidCuisineFactory : RecipeFactory
+    {
+        public override Sandwich CreateSandwich()
+        {
+            return new PBandJ();
+        }
+
+        public override Dessert CreateDessert()
+        {
+            return new IceCreamSundae();
+        }
+    }
+
+    class Order
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Who are you? (A)dult or (C)hild?");
+            char input = Console.ReadKey().KeyChar;
+            RecipeFactory factory;
+            switch (input)
+            {
+                case 'A':
+                    factory = new AdultCuisineFactory();
+                    break;
+
+                case 'C':
+                    factory = new KidCuisineFactory();
+                    break;
+
+                default:
+                    throw new NotImplementedException();
+
+            }
+
+            var sandwich = factory.CreateSandwich();
+            var dessert = factory.CreateDessert();
+
+            Console.WriteLine("\nSandwich: " + sandwich.GetType().Name);
+            Console.WriteLine("Dessert: " + dessert.GetType().Name);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LearningDesignPatterns/LearningDesignPatterns/Structural/Composite.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningDesignPatterns.Structural
+{
+    /// <summary>
+    /// Component abstract class
+    /// </summary>
+    public abstract class SoftDrink
+    {
+        public int Calories { get; set; }
+
+        public List<SoftDrink> Flavors { get; set; }
+
+        public SoftDrink(int calories)
+        {
+            Calories = calories;
+            Flavors = new List<SoftDrink>();
+        }
+
+        /// <summary>
+        /// "Flatten" method, returns all available flavors
+        /// </summary>
+        public void DisplayCalories()
+        {
+            Console.WriteLine(this.GetType().Name + ": " + this.Calories.ToString() + " calories.");
+            foreach (var drink in this.Flavors)
+            {
+                drink.DisplayCalories();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Leaf class
+    /// </summary>
+    public class VanillaCola : SoftDrink
+    {
+        public VanillaCola(int calories) : base(calories) { }
+    }
+
+    /// <summary>
+    /// Leaf class
+    /// </summary>
+    public class CherryCola : SoftDrink
+    {
+        public CherryCola(int calories) : base(calories) { }
+    }
+
+    /// <summary>
+    /// Leaf class
+    /// </summary>
+    public class StrawberryRootBeer : SoftDrink
+    {
+        public StrawberryRootBeer(int calories) : base(calories) { }
+    }
+
+    /// <summary>
+    /// Leaf class
+    /// </summary>
+    public class VanillaRootBeer : SoftDrink
+    {
+        public VanillaRootBeer(int calories) : base(calories) { }
+    }
+
+    /// <summary>
+    /// Composite class
+    /// </summary>
+    public class Cola : SoftDrink
+    {
+        public Cola(int calories) : base(calories) { }
+    }
+
+    /// <summary>
+    /// Leaf class
+    /// </summary>
+    public class LemonLime : SoftDrink
+    {
+        public LemonLime(int calories) : base(calories) { }
+    }
+
+    /// <summary>
+    /// Composite class
+    /// </summary>
+    public class RootBeer : SoftDrink
+    {
+        public RootBeer(int calories) : base(calories) { }
+    }
+
+    /// <summary>
+    /// Composite class, root node
+    /// </summary>
+    public class SodaWater : SoftDrink
+    {
+        public SodaWater(int calories) : base(calories) { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var colas = new Cola(210);
+            colas.Flavors.Add(new VanillaCola(215));
+            colas.Flavors.Add(new CherryCola(210));
+
+            var lemonLime = new LemonLime(185);
+
+            var rootBeers = new RootBeer(195);
+            rootBeers.Flavors.Add(new VanillaRootBeer(200));
+            rootBeers.Flavors.Add(new StrawberryRootBeer(200));
+
+            SodaWater sodaWater = new SodaWater(180);
+            sodaWater.Flavors.Add(colas);
+            sodaWater.Flavors.Add(lemonLime);
+            sodaWater.Flavors.Add(rootBeers);
+
+            sodaWater.DisplayCalories();
+
+            Console.ReadKey();
+        }
+    }
+}
